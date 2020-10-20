bbb557e6708e099d163965d9aecc7d1e1b5c2bae
Kristin <Kristin@KRISTIN-PC>
2020-10-20 12:55:26 +0300

    first commit

---
+++ Punktijaht/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace Punktijaht
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.KT_btn_Uusmäng = new System.Windows.Forms.Button();
+            this.KT_btn_Viskab_1 = new System.Windows.Forms.Button();
+            this.KT_btn_Viskab_2 = new System.Windows.Forms.Button();
+            this.KT_txt_Vise_1_1 = new System.Windows.Forms.TextBox();
+            this.KT_txt_Vise_1_2 = new System.Windows.Forms.TextBox();
+            this.KT_txt_Vise_2_2 = new System.Windows.Forms.TextBox();
+            this.KT_txt_Vise_2_1 = new System.Windows.Forms.TextBox();
+            this.KT_txt_Summa2 = new System.Windows.Forms.TextBox();
+            this.KT_txt_Summa1 = new System.Windows.Forms.TextBox();
+            this.KT_label_Nimi1 = new System.Windows.Forms.Label();
+            this.KT_lbl_Summa2 = new System.Windows.Forms.Label();
+            this.KT_lbl_Tulemus = new System.Windows.Forms.Label();
+            this.KT_txt_Tulemus = new System.Windows.Forms.TextBox();
+            this.KT_btn_Lopeta = new System.Windows.Forms.Button();
+            this.KT_pic_Juku = new System.Windows.Forms.PictureBox();
+            this.KT_pic_Juula = new System.Windows.Forms.PictureBox();
+            this.KT_pic_Voitja = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.KT_pic_Juku)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.KT_pic_Juula)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.KT_pic_Voitja)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // KT_btn_Uusmäng
+            // 
+            this.KT_btn_Uusmäng.BackColor = System.Drawing.Color.Moccasin;
+            this.KT_btn_Uusmäng.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.KT_btn_Uusmäng.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KT_btn_Uusmäng.Location = new System.Drawing.Point(306, 38);
+            this.KT_btn_Uusmäng.Name = "KT_btn_Uusmäng";
+            this.KT_btn_Uusmäng.Size = new System.Drawing.Size(164, 59);
+            this.KT_btn_Uusmäng.TabIndex = 0;
+            this.KT_btn_Uusmäng.Text = "Alusta uut mängu";
+            this.KT_btn_Uusmäng.UseVisualStyleBackColor = false;
+            this.KT_btn_Uusmäng.Click += new System.EventHandler(this.KT_btn_Uusmäng_Click);
+            // 
+            // KT_btn_Viskab_1
+            // 
+            this.KT_btn_Viskab_1.BackColor = System.Drawing.Color.RosyBrown;
+            this.KT_btn_Viskab_1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.KT_btn_Viskab_1.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KT_btn_Viskab_1.Location = new System.Drawing.Point(152, 140);
+            this.KT_btn_Viskab_1.Name = "KT_btn_Viskab_1";
+            this.KT_btn_Viskab_1.Size = new System.Drawing.Size(117, 44);
+            this.KT_btn_Viskab_1.TabIndex = 1;
+            this.KT_btn_Viskab_1.Text = "Mängib Juku";
+            this.KT_btn_Viskab_1.UseVisualStyleBackColor = false;
+            this.KT_btn_Viskab_1.Click += new System.EventHandler(this.KT_btn_Viskab_1_Click);
+            // 
+            // KT_btn_Viskab_2
+            // 
+            this.KT_btn_Viskab_2.BackColor = System.Drawing.Color.LightSalmon;
+            this.KT_btn_Viskab_2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.KT_btn_Viskab_2.Location = new System.Drawing.Point(480, 140);
+            this.KT_btn_Viskab_2.Name = "KT_btn_Viskab_2";
+            this.KT_btn_Viskab_2.Size = new System.Drawing.Size(117, 44);
+            this.KT_btn_Viskab_2.TabIndex = 2;
+            this.KT_btn_Viskab_2.Text = "Mängib Juula";
+            this.KT_btn_Viskab_2.UseVisualStyleBackColor = false;
+            this.KT_btn_Viskab_2.Click += new System.EventHandler(this.KT_btn_Viskab_2_Click);
+            // 
+            // KT_txt_Vise_1_1
+            // 
+            this.KT_txt_Vise_1_1.Location = new System.Drawing.Point(153, 207);
+            this.KT_txt_Vise_1_1.Multiline = true;
+            this.KT_txt_Vise_1_1.Name = "KT_txt_Vise_1_1";
+            this.KT_txt_Vise_1_1.Size = new System.Drawing.Size(53, 42);
+            this.KT_txt_Vise_1_1.TabIndex = 3;
+            this.KT_txt_Vise_1_1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // KT_txt_Vise_1_2
+            // 
+            this.KT_txt_Vise_1_2.Location = new System.Drawing.Point(217, 207);
+            this.KT_txt_Vise_1_2.Multiline = true;
+            this.KT_txt_Vise_1_2.Name = "KT_txt_Vise_1_2";
+            this.KT_txt_Vise_1_2.Size = new System.Drawing.Size(53, 42);
+            this.KT_txt_Vise_1_2.TabIndex = 4;
+            this.KT_txt_Vise_1_2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // KT_txt_Vise_2_2
+            // 
+            this.KT_txt_Vise_2_2.Location = new System.Drawing.Point(546, 207);
+            this.KT_txt_Vise_2_2.Multiline = true;
+            this.KT_txt_Vise_2_2.Name = "KT_txt_Vise_2_2";
+            this.KT_txt_Vise_2_2.Size = new System.Drawing.Size(53, 42);
+            this.KT_txt_Vise_2_2.TabIndex = 6;
+            this.KT_txt_Vise_2_2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // KT_txt_Vise_2_1
+            // 
+            this.KT_txt_Vise_2_1.Location = new System.Drawing.Point(482, 207);
+            this.KT_txt_Vise_2_1.Multiline = true;
+            this.KT_txt_Vise_2_1.Name = "KT_txt_Vise_2_1";
+            this.KT_txt_Vise_2_1.Size = new System.Drawing.Size(53, 42);
+            this.KT_txt_Vise_2_1.TabIndex = 5;
+            this.KT_txt_Vise_2_1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // KT_txt_Summa2
+            // 
+            this.KT_txt_Summa2.Location = new System.Drawing.Point(480, 326);
+            this.KT_txt_Summa2.Multiline = true;
+            this.KT_txt_Summa2.Name = "KT_txt_Summa2";
+            this.KT_txt_Summa2.Size = new System.Drawing.Size(117, 42);
+            this.KT_txt_Summa2.TabIndex = 7;
+            this.KT_txt_Summa2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // KT_txt_Summa1
+            // 
+            this.KT_txt_Summa1.Location = new System.Drawing.Point(152, 326);
+            this.KT_txt_Summa1.Multiline = true;
+            this.KT_txt_Summa1.Name = "KT_txt_Summa1";
+            this.KT_txt_Summa1.Size = new System.Drawing.Size(117, 42);
+            this.KT_txt_Summa1.TabIndex = 8;
+            this.KT_txt_Summa1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // KT_label_Nimi1
+            // 
+            this.KT_label_Nimi1.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KT_label_Nimi1.Location = new System.Drawing.Point(153, 277);
+            this.KT_label_Nimi1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.KT_label_Nimi1.Name = "KT_label_Nimi1";
+            this.KT_label_Nimi1.Size = new System.Drawing.Size(116, 46);
+            this.KT_label_Nimi1.TabIndex = 9;
+            this.KT_label_Nimi1.Text = "Juku punktid";
+            this.KT_label_Nimi1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // KT_lbl_Summa2
+            // 
+            this.KT_lbl_Summa2.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KT_lbl_Summa2.Location = new System.Drawing.Point(481, 277);
+            this.KT_lbl_Summa2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.KT_lbl_Summa2.Name = "KT_lbl_Summa2";
+            this.KT_lbl_Summa2.Size = new System.Drawing.Size(118, 46);
+            this.KT_lbl_Summa2.TabIndex = 10;
+            this.KT_lbl_Summa2.Text = "Juula punktid";
+            this.KT_lbl_Summa2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // KT_lbl_Tulemus
+            // 
+            this.KT_lbl_Tulemus.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KT_lbl_Tulemus.Location = new System.Drawing.Point(314, 392);
+            this.KT_lbl_Tulemus.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.KT_lbl_Tulemus.Name = "KT_lbl_Tulemus";
+            this.KT_lbl_Tulemus.Size = new System.Drawing.Size(120, 38);
+            this.KT_lbl_Tulemus.TabIndex = 11;
+            this.KT_lbl_Tulemus.Text = "Tulemus";
+            this.KT_lbl_Tulemus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // KT_txt_Tulemus
+            // 
+            this.KT_txt_Tulemus.Location = new System.Drawing.Point(277, 433);
+            this.KT_txt_Tulemus.Multiline = true;
+            this.KT_txt_Tulemus.Name = "KT_txt_Tulemus";
+            this.KT_txt_Tulemus.Size = new System.Drawing.Size(193, 38);
+            this.KT_txt_Tulemus.TabIndex = 12;
+            this.KT_txt_Tulemus.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // KT_btn_Lopeta
+            // 
+            this.KT_btn_Lopeta.BackColor = System.Drawing.Color.CadetBlue;
+            this.KT_btn_Lopeta.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.KT_btn_Lopeta.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KT_btn_Lopeta.Location = new System.Drawing.Point(571, 557);
+            this.KT_btn_Lopeta.Name = "KT_btn_Lopeta";
+            this.KT_btn_Lopeta.Size = new System.Drawing.Size(107, 43);
+            this.KT_btn_Lopeta.TabIndex = 13;
+            this.KT_btn_Lopeta.Text = "Lõpeta";
+            this.KT_btn_Lopeta.UseVisualStyleBackColor = false;
+            this.KT_btn_Lopeta.Click += new System.EventHandler(this.KT_btn_Lopeta_Click);
+            // 
+            // KT_pic_Juku
+            // 
+            this.KT_pic_Juku.ErrorImage = null;
+            this.KT_pic_Juku.Image = global::Punktijaht.Properties.Resources.Juku;
+            this.KT_pic_Juku.InitialImage = null;
+            this.KT_pic_Juku.Location = new System.Drawing.Point(24, 103);
+            this.KT_pic_Juku.Name = "KT_pic_Juku";
+            this.KT_pic_Juku.Size = new System.Drawing.Size(87, 100);
+            this.KT_pic_Juku.TabIndex = 15;
+            this.KT_pic_Juku.TabStop = false;
+            // 
+            // KT_pic_Juula
+            // 
+            this.KT_pic_Juula.ErrorImage = null;
+            this.KT_pic_Juula.Image = global::Punktijaht.Properties.Resources.Juula;
+            this.KT_pic_Juula.InitialImage = null;
+            this.KT_pic_Juula.Location = new System.Drawing.Point(634, 103);
+            this.KT_pic_Juula.Name = "KT_pic_Juula";
+            this.KT_pic_Juula.Size = new System.Drawing.Size(87, 100);
+            this.KT_pic_Juula.TabIndex = 14;
+            this.KT_pic_Juula.TabStop = false;
+            // 
+            // KT_pic_Voitja
+            // 
+            this.KT_pic_Voitja.Location = new System.Drawing.Point(317, 487);
+            this.KT_pic_Voitja.Name = "KT_pic_Voitja";
+            this.KT_pic_Voitja.Size = new System.Drawing.Size(117, 113);
+            this.KT_pic_Voitja.TabIndex = 16;
+            this.KT_pic_Voitja.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(762, 626);
+            this.Controls.Add(this.KT_pic_Voitja);
+            this.Controls.Add(this.KT_pic_Juku);
+            this.Controls.Add(this.KT_pic_Juula);
+            this.Controls.Add(this.KT_btn_Lopeta);
+            this.Controls.Add(this.KT_txt_Tulemus);
+            this.Controls.Add(this.KT_lbl_Tulemus);
+            this.Controls.Add(this.KT_lbl_Summa2);
+            this.Controls.Add(this.KT_label_Nimi1);
+            this.Controls.Add(this.KT_txt_Summa1);
+            this.Controls.Add(this.KT_txt_Summa2);
+            this.Controls.Add(this.KT_txt_Vise_2_2);
+            this.Controls.Add(this.KT_txt_Vise_2_1);
+            this.Controls.Add(this.KT_txt_Vise_1_2);
+            this.Controls.Add(this.KT_txt_Vise_1_1);
+            this.Controls.Add(this.KT_btn_Viskab_2);
+            this.Controls.Add(this.KT_btn_Viskab_1);
+            this.Controls.Add(this.KT_btn_Uusmäng);
+            this.Font = new System.Drawing.Font("Courier New", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Punktijaht";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.KT_pic_Juku)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.KT_pic_Juula)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.KT_pic_Voitja)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button KT_btn_Uusmäng;
+        private System.Windows.Forms.Button KT_btn_Viskab_1;
+        private System.Windows.Forms.Button KT_btn_Viskab_2;
+        private System.Windows.Forms.TextBox KT_txt_Vise_1_1;
+        private System.Windows.Forms.TextBox KT_txt_Vise_1_2;
+        private System.Windows.Forms.TextBox KT_txt_Vise_2_2;
+        private System.Windows.Forms.TextBox KT_txt_Vise_2_1;
+        private System.Windows.Forms.TextBox KT_txt_Summa2;
+        private System.Windows.Forms.TextBox KT_txt_Summa1;
+        private System.Windows.Forms.Label KT_label_Nimi1;
+        private System.Windows.Forms.Label KT_lbl_Summa2;
+        private System.Windows.Forms.Label KT_lbl_Tulemus;
+        private System.Windows.Forms.TextBox KT_txt_Tulemus;
+        private System.Windows.Forms.Button KT_btn_Lopeta;
+        private System.Windows.Forms.PictureBox KT_pic_Juula;
+        private System.Windows.Forms.PictureBox KT_pic_Juku;
+        private System.Windows.Forms.PictureBox KT_pic_Voitja;
+    }
+}
+
---
+++ Punktijaht/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Punktijaht
+{
+    public partial class Form1 : Form
+    {
+        int juhuarv1, juhuarv2, juhuarv3, juhuarv4, tulemus1, tulemus2;
+        int mitu = 2, minArv = 1, maxArv = 6;        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            KT_btn_Uusmäng.Enabled = true;
+            KT_btn_Viskab_1.Enabled = false;
+            KT_btn_Viskab_2.Enabled = false;
+        }
+
+        private void KT_btn_Uusmäng_Click(object sender, EventArgs e)
+        {
+            KT_txt_Vise_1_1.Text = "";
+            KT_txt_Vise_1_2.Text = "";
+            KT_txt_Vise_2_1.Text = "";
+            KT_txt_Vise_2_2.Text = "";
+            KT_txt_Summa1.Text = "";
+            KT_txt_Summa2.Text = "";
+            KT_txt_Tulemus.Text = "";
+            KT_pic_Voitja.Image = null;
+            KT_btn_Viskab_1.Enabled = true;
+        }
+
+        private void KT_btn_Viskab_1_Click(object sender, EventArgs e)
+        {
+            Random punktid = new Random();
+
+            for (int i = 1; i < mitu; i++)
+            {
+                juhuarv1 = punktid.Next(minArv, maxArv + 1);
+                KT_txt_Vise_1_1.Text = juhuarv1.ToString();
+                KT_txt_Vise_1_1.TabStop = false;
+            }
+
+            for (int i = 1; i < mitu; i++)
+            {
+                juhuarv2 = punktid.Next(minArv, maxArv + 1);
+                KT_txt_Vise_1_2.Text = juhuarv2.ToString();
+                KT_txt_Vise_1_2.TabStop = false;
+            }
+
+            tulemus1 = juhuarv1 + juhuarv2;
+            KT_txt_Summa1.Text = (tulemus1).ToString();
+            KT_txt_Summa1.TabStop = false;
+            KT_btn_Uusmäng.Enabled = false;
+            KT_btn_Viskab_1.Enabled = false;
+            KT_btn_Viskab_2.Enabled = true;
+        }
+
+        private void KT_btn_Viskab_2_Click(object sender, EventArgs e)
+        {
+            Random punktid = new Random();
+
+            int mitu = 2, minArv = 1, maxArv = 6;
+            for (int i = 1; i < mitu; i++)
+            {
+                juhuarv3 = punktid.Next(minArv, maxArv + 1);
+                KT_txt_Vise_2_1.Text = juhuarv3.ToString();
+                KT_txt_Vise_2_1.TabStop = false;
+            }
+            for (int i = 1; i < mitu; i++)
+            {
+                juhuarv4 = punktid.Next(minArv, maxArv + 1);
+                KT_txt_Vise_2_2.Text = juhuarv4.ToString();
+                KT_txt_Vise_2_2.TabStop = false;
+            }
+            tulemus2 = juhuarv3 + juhuarv4;
+            KT_txt_Summa2.Text = (tulemus2).ToString();
+            KT_txt_Summa2.TabStop = false;
+
+            if (tulemus1 > tulemus2)
+            {
+                KT_txt_Tulemus.Text = "Võitis Juku!";
+                KT_txt_Tulemus.TabStop = false;
+                KT_pic_Voitja.Image = global::Punktijaht.Properties.Resources.Juku;
+            }
+            else if (tulemus1 < tulemus2)
+            {
+                KT_txt_Tulemus.Text = "Võitis Juula!";
+                KT_txt_Tulemus.TabStop = false;
+                KT_pic_Voitja.Image = global::Punktijaht.Properties.Resources.Juula;
+            }
+            else
+            {
+                KT_txt_Tulemus.Text = "Mäng jäi viiki!";
+                KT_txt_Tulemus.TabStop = false;
+                KT_pic_Voitja.Image = global::Punktijaht.Properties.Resources.Viik;
+            }
+
+            KT_btn_Viskab_2.Enabled = false;
+            KT_btn_Uusmäng.Enabled = true;
+        }
+
+        private void KT_btn_Lopeta_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Kas oled kindel, et soovid lõpetada?", "Lõpeta mäng",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Information) ==
+                DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+    }
+}
